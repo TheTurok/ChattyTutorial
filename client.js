8791ea4eb63ce6f833b20f5a6cb145023ea86e29
Timothy Choi <TurokChoi@gmail.com>
2018-05-31 17:54:57 -0700

    finished basic functionality

--- client.js
+++ client.js
@@ -17,11 +17,46 @@ new Vue({
 	created: function(){
 		//if server emits 'userjoined' then update connected users array
 		socket.on('userjoined',function(socketId){
-			this.onlineUsers.push(socketId);
+
+			//get already connected users first
+			axios.get('/onlineusers')
+				.then(function(response){
+					for(var key in response.data){
+						if(this.onlineUsers.indexOf(key) <= -1){
+							this.onlineUsers.push(key);
+						}
+					}
+					console.log(this.onlineUsers);
+				}.bind(this));
+
+			
 		}.bind(this));
+
+		//iff server bporadcast user left
+		socket.on('userleft', function(socketId){
+			var index = this.onlineUsers.indexOf(socketId);
+			if(index > -1){ // 0 or more
+				this.onlineUsers.splice(index, 1);
+			}
+		}.bind(this)); //don't forget to bind
+
+		//update chat message
+		socket.on('chatmessage',function(message){
+			this.messages.push(message);
+		}.bind(this));
+
 	},
 	methods:{
 		send: function(){
+			this.message.type = "chat";
+			this.message.user = socket.id;
+			this.message.timestamp = "Today";
+			socket.emit('chatmessage', this.message);
+
+			this.message.type = ''; //reset
+			this.message.user = '';
+			this.message.text = '';
+			this.message.timestamp = '';
 
 		},
 		userIsTyping: function(username){
--- index.js
+++ index.js
@@ -17,14 +17,27 @@ app.get('/', function (request,response){
 	response.sendFile(__dirname + '/index.html');
 });
 
+app.get('/onlineusers', function(request,response){
+	response.send(io.sockets.adapter.rooms);
+});
+
 io.on('connection', function(socket) { //on every connection do this
 	console.log('A user connected:' + socket.id);
 
 	//tell all clients someone connected
 	io.emit('userjoined', socket.id); //with their id
 
+	//cleint send message
+	socket.on('chatmessage', function(message){
+		//emit this event to all clients
+		io.emit('chatmessage', message);
+	});
+
+
 	socket.on('disconnect', function(){ //on idsconnect
 		console.log('User Lef:t' + socket.id)
+		//tell clients someone disconnected
+		socket.broadcast.emit('userleft', socket.id);
 	});
 
 }); 
